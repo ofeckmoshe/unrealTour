1ed687e3f1890d8780e957a5adcbe9c16791e739
ofeck <ofeckm3@gmail.com>
2020-01-28 16:25:44 +0200

    Update UserPage.js

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -10,6 +10,8 @@ import Gallery from "./Gallery";
 class UserPage extends Component {
     constructor(props) {
         super(props);
+        this.uploaded = React.createRef()
+        this.wish = React.createRef() 
         this.state = {
             user: Cookie.get('user') ? JSON.parse(Cookie.get('user')) : null,
             formOpen: false,
@@ -30,7 +32,6 @@ class UserPage extends Component {
     
 
     onSuccess = (apt) => {
-        // console.log(apt)
         this.setState({
             apartments:  apt,
             
@@ -58,9 +59,9 @@ class UserPage extends Component {
 
     };
 
-    hendleSelect = (e) => {
-        const name = e.target.name;
-    };
+    // hendleSelect = (e) => {
+    //     const name = e.target.name;
+    // };
 
     upload = (e) => {
         e.preventDefault()
@@ -116,11 +117,12 @@ class UserPage extends Component {
     }
 
     myUpload = () =>{
-        this.setState({myUpload:!this.state.myUpload})
+        this.setState({myUpload:!this.state.myUpload},()=>{this.state.myUpload&&window.scrollTo(0, this.uploaded.current.offsetTop)})
+        
     }
 
     myWishList = () =>{
-        this.setState({myWishList:!this.state.myWishList})
+        this.setState({myWishList:!this.state.myWishList},()=>{this.state.myWishList&&window.scrollTo(0, this.wish.current.offsetTop)})
     }
 
     render() {
@@ -132,26 +134,29 @@ class UserPage extends Component {
             citiesArr.push(this.state.cities[city].name);
         }
         return (
-            <Container>
+            <Row>
+                <div style={{display:'flex',flexDirection:'column',backgroundColor:'lightgray',padding:'0'}} className={'col-2'}>
+                    <Button variant="info" onClick={this.openForm} style={{float:'left',display:'block'}}>
+                        add apartment</Button>
+                    <Button variant="info" onClick={this.myUpload} style={{float:'left'}}>
+                        Your apartments</Button>
+                    <Button variant="info" onClick={this.myWishList} style={{float:'left'}}>
+                    Your wish list</Button></div>
+            
+            <Container className={'col-10'}>
             <Container 
             style={{ fontFamily: 'Architects Daughter', textAlign: 'center', backgroundImage: "url(./images/realestate-aidturk.jpg)",backgroundSize:'cover' ,height:'550px',position:'relative'}}>
                 <h1 style={{textShadow:'2px 2px 2px lightseagreen'}}>
                     Hello {this.state.user.first_name}, Good to see you again</h1>
-                <div style={{display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
-                <Button variant="info" onClick={this.openForm} style={{float:'left',display:'block'}}>
-                    add apartment</Button>
-                <Button variant="info" onClick={this.myUpload} style={{float:'left'}}>
-                    Your apartments</Button>
-                <Button variant="info" onClick={this.myWishList} style={{float:'left'}}>
-                    Your wish list</Button></div>
+
                 {this.state.formOpen &&
-                    <Form style={{backgroundColor:'aliceblue',padding:'15px'}}>
+                    <Form style={{backgroundColor:'aliceblue',padding:'15px'}} onSubmit={this.upload}>
                         <Form.Row>
                             <Form.Group as={Col} controlId="formGridState">
                                 <Form.Label>City</Form.Label>
-                                <Form.Control as="select" name={"city"}
+                                <Form.Control as="select" name={"city"} required
                                     onSelect={this.hendleChanges} onChange={this.hendleChanges}>
-                                    <option value={""} >Choose...</option>
+                                    <option value={""} ></option>
                                     {citiesArr.map((city, i) => {
                                         return (
                                             <option key={i} value={city}>{city}</option>
@@ -163,7 +168,7 @@ class UserPage extends Component {
                             <Form.Group as={Col} controlId="formGridAddress">
                                 <Form.Label>Address</Form.Label>
                                 <Form.Control placeholder="Enter your apartment address" name={"address"}
-                                    onChange={this.hendleChanges} />
+                                    required onChange={this.hendleChanges} />
                             </Form.Group>
                         </Form.Row>
 
@@ -171,32 +176,32 @@ class UserPage extends Component {
                             <Form.Group as={Col} controlId="formGridPrice">
                                 <Form.Label>Price</Form.Label>
                                 <Form.Control type="text" placeholder="Enter Price" name={"price"}
-                                    onChange={this.hendleChanges} />
+                                    required onChange={this.hendleChanges} />
                             </Form.Group>
 
                             <Form.Group as={Col} controlId="formGridRooms">
                                 <Form.Label>Number of Rooms</Form.Label>
                                 <Form.Control type="number" name={"number_of_room"}
-                                    onChange={this.hendleChanges} />
+                                    required onChange={this.hendleChanges} />
                             </Form.Group>
 
                             <Form.Group as={Col} controlId="formGridBaths">
                                 <Form.Label>Number of Bath</Form.Label>
                                 <Form.Control type="number" name={"number_of_bath"}
-                                    onChange={this.hendleChanges} />
+                                    required onChange={this.hendleChanges} />
                             </Form.Group>
 
                             <Form.Group as={Col} controlId="formGridSqft">
                                 <Form.Label>Sqft</Form.Label>
                                 <Form.Control type="text" name={"sqft"}
-                                    onChange={this.hendleChanges} />
+                                    required onChange={this.hendleChanges} />
                             </Form.Group>
                         </Form.Row>
 
                         <Form.Row>
                         <Form.Group as={Col} controlId="formGridState">
                                 <Form.Label>property type</Form.Label>
-                                <Form.Control as="select" name={"property_type"}
+                                <Form.Control as="select" name={"property_type"} required
                                     onSelect={this.hendleChanges} onChange={this.hendleChanges}>
                                     <option value={""} >select</option>
                                     <option value={"house"} >house</option>
@@ -207,12 +212,12 @@ class UserPage extends Component {
                             </Form.Group>
                             <Form.Group as={Col} controlId="formGridDescription">
                                 <Form.Label>Description</Form.Label>
-                                <Form.Control type="text" name={"description"}
+                                <Form.Control type="text" name={"description"} required
                                     onChange={this.hendleChanges} />
                             </Form.Group>
                             <Form.Group as={Col} controlId="formGridState">
                                 <Form.Label>Rent / Sale</Form.Label>
-                                <Form.Control as="select" name={"sale_status"}
+                                <Form.Control as="select" name={"sale_status"} required
                                     onSelect={this.hendleChanges} onChange={this.hendleChanges}>
                                     <option value={""} >select</option>
                                     <option value={"rent"} >rent</option>
@@ -222,19 +227,20 @@ class UserPage extends Component {
                             </Form.Group>
                             <Form.Group as={Col} controlId="formGridState">
                                 <Form.Label>main image</Form.Label>
-                                <Form.Control type="file" className="main_image" name={"main_image"} onChange={this.fileUpload}>
+                                <Form.Control type="file" className="main_image" name={"main_image"} required onChange={this.fileUpload}>
                                 </Form.Control>
                             </Form.Group>
                             <Form.Group as={Col} controlId="formGridState">
                                 <Form.Label>Images</Form.Label>
-                                <Form.Control type="file" className="Images" name={"Images"} onChange={this.filesUpload} multiple>
+                                <Form.Control type="file" className="Images" name={"Images"} required onChange={this.filesUpload} multiple>
                                 </Form.Control>
                             </Form.Group>
                         </Form.Row>
-                        <Button variant={this.state.counter == 11 ?"primary" :"light"} type="submit" 
+                        {/* <Button variant={this.state.counter == 11 ?"primary" :"light"} type="submit" 
                                 onClick={this.upload} disabled={this.state.counter == 11 ? false: true}>
                             Submit
-                    </Button>
+                    </Button> */}
+                      <input type="submit" onSubmit={this.upload}/>
                     </Form>}
                     <Row style={{position:'absolute',bottom:0}}>
                         <h1 className={'col-12'} style={{textShadow:'2px 2px 2px lightseagreen'}}>you have uploaded  - {this.state.apartments?this.state.apartments.length:'0'} apartments,</h1>
@@ -242,7 +248,7 @@ class UserPage extends Component {
                     </Row>
             </Container>
             {this.state.apartments && this.state.myUpload &&
-            <div id={'apartmentDeck'} className={'row card-deck cards d-flex justify-content-center'} style={{alignItems:"center",padding:'10px',overflowX:'auto'}}>
+            <div ref={this.uploaded} id={'apartmentDeck'} className={'row card-deck cards d-flex justify-content-center'} style={{alignItems:"center",padding:'10px',overflowX:'auto'}}>
                     <h3 className={'col-12'}>your uploaded apartments list - {this.state.apartments.length} apartments is available</h3>
                     {this.state.apartments.map((item,i) => {
                         return (
@@ -252,7 +258,7 @@ class UserPage extends Component {
                     }
                 </div>}
             {this.state.wishList && this.state.myWishList &&
-            <div id={'apartmentDeck'} className={'row card-deck cards d-flex justify-content-start'} style={{padding:'10px',overflowX:'auto'}}>
+            <div ref={this.wish} id={'apartmentDeck'} className={'row card-deck cards d-flex justify-content-start'} style={{padding:'10px',overflowX:'auto'}}>
                     <h3 className={'col-12'}>your wish list - {this.state.wishList.length} apartments is available</h3>
                     {this.state.wishList.map((item,i) => {
                         return (
@@ -262,6 +268,7 @@ class UserPage extends Component {
                     }
                 </div>}
             </Container>
+            </Row>
         );
     }
 }
